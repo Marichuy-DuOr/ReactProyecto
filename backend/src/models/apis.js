2d6186176747d0cc56419ff5e015fab17eec0aa7
Mary Chuy <maria.0299.ornelas@gmail.com>
2021-06-15 00:55:37 -0500

    Perfil de usuario

--- backend/src/models/apis.js
+++ backend/src/models/apis.js
@@ -2,49 +2,49 @@ const axios = require('axios')
 module.exports = {
 
     getRandomSpoonacular: async() => {
-        return await axios.get(`https://api.spoonacular.com/recipes/random?number=12&apiKey=0ee5f89c8bb54cfe9dc6fa23c96f8e1f`)
+        return await axios.get(`https://api.spoonacular.com/recipes/random?number=12&apiKey=dae0e867ef194bab8840bc354a3102bc`)
             .then(data => data)
             .catch(err => { console.log(err); return err })
 
     },
 
     getRecipeSpoonacular: async(req) => {
-        return await axios.get(`https://api.spoonacular.com/recipes/${req}/information?includeNutrition=true&apiKey=0ee5f89c8bb54cfe9dc6fa23c96f8e1f`)
+        return await axios.get(`https://api.spoonacular.com/recipes/${req}/information?includeNutrition=true&apiKey=dae0e867ef194bab8840bc354a3102bc`)
             .then(data => data)
             .catch(err => { console.log(err); return err })
 
     },
 
     getIngredientsSpoonacular: async(req) => {
-        return await axios.get(`https://api.spoonacular.com/food/ingredients/search?query=${req}&number=12&apiKey=0ee5f89c8bb54cfe9dc6fa23c96f8e1f`)
+        return await axios.get(`https://api.spoonacular.com/food/ingredients/search?query=${req}&number=12&apiKey=dae0e867ef194bab8840bc354a3102bc`)
             .then(data => data)
             .catch(err => { console.log(err); return err })
 
     },
 
     getIngredientSpoonacular: async(req) => {
-        return await axios.get(`https://api.spoonacular.com/food/ingredients/${req}/information?amount=1&apiKey=0ee5f89c8bb54cfe9dc6fa23c96f8e1f`)
+        return await axios.get(`https://api.spoonacular.com/food/ingredients/${req}/information?amount=1&apiKey=dae0e867ef194bab8840bc354a3102bc`)
             .then(data => data)
             .catch(err => { console.log(err); return err })
 
     },
 
     getRecipeQuerySpoonacular: async(req) => {
-        return await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${req}&number=12&apiKey=0ee5f89c8bb54cfe9dc6fa23c96f8e1f`)
+        return await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${req}&number=12&apiKey=dae0e867ef194bab8840bc354a3102bc`)
             .then(data => data)
             .catch(err => { console.log(err); return err })
 
     },
 
     getRecipesSpoonacular: async(req) => {
-        return await axios.get(`https://api.spoonacular.com/recipes/informationBulk?apiKey=0ee5f89c8bb54cfe9dc6fa23c96f8e1f=${req}`)
+        return await axios.get(`https://api.spoonacular.com/recipes/informationBulk?apiKey=dae0e867ef194bab8840bc354a3102bc=${req}`)
             .then(data => data)
             .catch(err => { console.log(err); return err })
 
     },
 
     getSimilarSpoonacular: async(req) => {
-        return await axios.get(`https://api.spoonacular.com/recipes/${req}/similar?number=12&apiKey=0ee5f89c8bb54cfe9dc6fa23c96f8e1f`)
+        return await axios.get(`https://api.spoonacular.com/recipes/${req}/similar?number=12&apiKey=dae0e867ef194bab8840bc354a3102bc`)
             .then(data => data)
             .catch(err => { console.log(err); return err })
 
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,10 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {useAuth} from '../contexts/Auth';
+import {
+  Avatar,
+} from 'react-native-paper';
 
 export default function DetailsScreen() {
+  const auth = useAuth();
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Profile Screen</Text>
+      <View style={{marginTop:50}}>
+        <Text style={styles.title}>{auth.authData.nombre} </Text>
+        <Text style={styles.title}>{auth.authData.apepat} {auth.authData.apemat}</Text>
+        <View style={styles.categoryContainer}>
+          <TouchableOpacity style={styles.categoryBtn}>
+            <View style={styles.categoryIcon}>
+              <Image source={require('../../assets/user.png')} width='300' height='300' />
+            </View>
+            <Text style={styles.categoryBtnTxt}>{auth.authData.email}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
+      
     );
   }
+
+  const styles = StyleSheet.create({
+    title: {
+      fontSize: 30,
+      marginTop: 3,
+      fontWeight: 'bold',
+      textAlign: 'center'
+    },
+    categoryContainer: {
+      flexDirection: 'row',
+      width: '90%',
+      alignSelf: 'center',
+      marginTop: 25,
+      marginBottom: 10,
+    },
+    categoryBtn: {
+      flex: 1,
+      width: '30%',
+      marginHorizontal: 0,
+      alignSelf: 'center',
+    },
+    categoryIcon: {
+      borderWidth: 0,
+      alignItems: 'center',
+      justifyContent: 'center',
+      alignSelf: 'center',
+      width: 350,
+      height: 350,
+      backgroundColor: '#fdeae7' /* '#FF6347' */,
+      borderRadius: 50,
+    },
+    categoryBtnTxt: {
+      alignSelf: 'center',
+      marginTop: 5,
+      color: '#de4f35',
+    },
+  });
